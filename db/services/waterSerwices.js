825eee75999506ddbd8472ba77bda31c2d56138f
Black Chaos <black.chaos.no1@gmail.com>
2024-01-28 23:52:16 +0200

    feat: add update entry

--- db/services/waterSerwices.js
+++ db/services/waterSerwices.js
@@ -1,47 +1,75 @@
-const { Types } = require("mongoose");
-const { HttpError } = require("../../helpers");
-const { Water } = require("../models/water")
+const { Types } = require('mongoose');
+const { HttpError } = require('../../helpers');
+const { Water } = require('../models/water');
 
 exports.add = async (data, owner) => {
     const date = new Date();
     const waterList = await Water.findOne({
-        date: { $gte: new Date(date.getFullYear(), date.getMonth(), date.getDate()) }, owner
+        date: { $gte: new Date(date.getFullYear(), date.getMonth(), date.getDate()) },
+        owner,
     });
     if (waterList) {
-        return Water.findByIdAndUpdate(waterList._id, {
-            $inc: { totalVolume: +data.waterVolume },
-            $push: { dailyEntries: data },
-        }, {new: true});
+        return Water.findByIdAndUpdate(
+            waterList._id,
+            {
+                $inc: { totalVolume: +data.waterVolume },
+                $push: { dailyEntries: data },
+            },
+            { new: true }
+        );
     }
     return Water.create({
         date,
         waterRate: owner.waterRate,
         totalVolume: data.waterVolume,
-        dailyEntries: [data] ,
+        dailyEntries: [data],
         owner,
     });
-}
+};
 
 exports.getCurrentDay = async owner => {
     const date = new Date();
-    const waterList = await Water.findOne({ date: { $gte: new Date(date.getFullYear(), date.getMonth(), date.getDate()) } });
+    const waterList = await Water.findOne({
+        date: { $gte: new Date(date.getFullYear(), date.getMonth(), date.getDate()) }, owner
+    });
     if (!waterList) {
-        throw HttpError(404, 'No entries for the day')
-        // return Water.create({
-        //     date,
-        //     waterRate: owner.waterRate,
-        //     totalVolume: 0,
-        //     dailyEntries: [],
-        //     owner,
-        // });
+        throw HttpError(404, 'No entries for the day');
     }
-    return waterList
-}
+    return waterList;
+};
 
-exports.remove = async (dayId, entryId, owner) => {
-    const day = await Water.findOne({ _id: dayId, owner })
-    if (!day) throw HttpError(404, 'Not entry of day')
+exports.update = async (dayId, entryId, data) => {
+    const day = await Water.findById(dayId);
+    if (!day) throw HttpError(404, 'Day entry does not exist');
     const entry = day.dailyEntries.find(i => i._id.toString() === entryId);
+    if (!entry) throw HttpError(404, 'Entry not found')
+    return Water.findOneAndUpdate(
+{ _id: dayId, 'dailyEntries._id': entryId },
+{
+    $inc: { totalVolume: +(data.waterVolume-entry.waterVolume) },
+            $set: {
+                'dailyEntries.$.waterVolume': data.waterVolume,
+                'dailyEntries.$.time': data.time,
+            },
+        }, {new: true}
+    );
+    // return Water.updateOne(
+        //     { _id: dayId, 'dailyEntries._id': entryId },
+        //     {
+    //         $inc: { totalVolume: +(data.waterVolume-entry.waterVolume) },
+    //         $set: {
+    //             'dailyEntries.$.waterVolume': data.waterVolume,
+    //             'dailyEntries.$.time': data.time,
+    //         },
+    //     }
+    // );
+};
+
+exports.remove = async (dayId, entryId) => {
+    const day = await Water.findById(dayId);
+    if (!day) throw HttpError(404, 'Day entry does not exist');
+    const entry = day.dailyEntries.find(i => i._id.toString() === entryId);
+    if (!entry) throw HttpError(404, 'Entry not found')
     return Water.findByIdAndUpdate(
         dayId,
         {
@@ -50,11 +78,13 @@ exports.remove = async (dayId, entryId, owner) => {
         },
         { new: true }
     );
-}
+};
 
-exports.checkById = async (id, owner) => {
-        const idIsValid = Types.ObjectId.isValid(id);
+exports.checkById = async (id) => {
+    const idIsValid = Types.ObjectId.isValid(id);
     if (!idIsValid) throw HttpError(404, 'Not valid id');
-    const isDayEntryExist = await Water.exists({ _id: id, owner })
-    if (!isDayEntryExist) throw HttpError(404, 'Not day entry exist')
+};
+exports.checkIdByOwner = async (id, owner) => {
+    const isDayEntryExist = await Water.exists({ _id: id, owner });
+    if (!isDayEntryExist) throw HttpError(404, 'Day entry does not exist');
 }
--- middlewares/waterMiddleware.js
+++ middlewares/waterMiddleware.js
@@ -2,6 +2,8 @@ const { waterServices } = require("../db/services");
 const { ctrlWrapper } = require("../helpers");
 
 exports.checkDayEntryById = ctrlWrapper(async (req, res, next) => {
-    await waterServices.checkById(req.params.dayId, req.user);
+    await waterServices.checkById(req.params.dayId);
+    await waterServices.checkById(req.params.entryId);
+    await waterServices.checkIdByOwner(req.params.dayId, req.user)
     next()
 })
