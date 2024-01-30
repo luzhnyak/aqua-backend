027c525356b4f6b722ef521613dc084d9ba71d7c
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2024-01-30 14:55:17 +0200

    add progress to service add

--- db/models/water.js
+++ db/models/water.js
@@ -1,60 +1,79 @@
-const { Schema, model } = require('mongoose');
-const { handleMongooseError } = require('../../helpers');
-const { regexp } = require('../../vars');
+const { Schema, model } = require("mongoose");
+const { handleMongooseError } = require("../../helpers");
+const { regexp } = require("../../vars");
 
-const entriesSchema = new Schema( {
-                waterVolume: {
-                    type: Number,
-                    required: [true, 'Set capacity of water'],
-                },
-                time: {
-                    type: String,
-                    match: regexp.time,
-                    required: [true, 'Time is required'],
-                },
-            },)
+const entriesSchema = new Schema({
+  waterVolume: {
+    type: Number,
+    required: [true, "Set capacity of water"],
+  },
+  time: {
+    type: String,
+    match: regexp.time,
+    required: [true, "Time is required"],
+  },
+});
 
 const waterSchema = new Schema(
-    {
-        date: {
-            type: Date,
-            required: [true, 'Date is required']
-        },
-        waterRate: {
-            type: Number,
-            default: 1500,
-            required: [true, 'Water rate is required'],
-        },
-        totalVolume: {
-            type: Number,
-            default: 0,
-            required: [true, 'Total is required']
-        },
-        progress: {
-            type: Number,
-            default: 0,
-            required: [true, 'Progress is required']
-        },
-        dailyEntries: [entriesSchema],
-        owner: {
-            type: Schema.Types.ObjectId,
-            ref: 'user',
-            required: true,
-        },
+  {
+    date: {
+      type: Date,
+      required: [true, "Date is required"],
     },
-    { versionKey: false, timestamps: true }
+    waterRate: {
+      type: Number,
+      default: 1500,
+      required: [true, "Water rate is required"],
+    },
+    totalVolume: {
+      type: Number,
+      default: 0,
+      required: [true, "Total is required"],
+    },
+    progress: {
+      type: Number,
+      default: 0,
+      required: [true, "Progress is required"],
+    },
+    dailyEntries: [entriesSchema],
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+  },
+  { versionKey: false, timestamps: true }
 );
 
 // waterSchema.methods.getPercent = function () {
 //     this.progress = (this.totalVolume / this.waterRate) * 100;
 //     console.log('this.progress:', this.progress);
 // };
-waterSchema.pre(['save', 'updateOne', 'findOneAndUpdate', 'findByIdAndUpdate'], function (next) {
-    this.progress = (this.totalVolume / this.waterRate) * 100;
-    console.log('this.progress:', this.progress);
-    next();
-});
+// waterSchema.pre(["create", "save"], function (next) {
+//   //   console.log(this);
+//   this.progress = (this.totalVolume / this.waterRate) * 100;
+//   console.log("this.progress create:", this.progress);
+//   next();
+// });
 
-waterSchema.post('save', handleMongooseError);
+// waterSchema.pre(
+//   ["updateOne", "findOneAndUpdate", "findByIdAndUpdate"],
+//   function (next) {
+//     // console.log(this.updatedUser);
+//     const updatedData = this.getUpdate();
 
-exports.Water = model('water', waterSchema);
+//     if (updatedData.$set && updatedData.$set.progress) {
+//       updatedData.$set.progress = 50;
+//       // (updatedData.$set.totalVolume / updatedData.$set.waterRate) * 100;
+//     }
+
+//     console.log(updatedData);
+//     console.log("this.progress: update", updatedData.$set);
+
+//     next();
+//   }
+// );
+
+waterSchema.post("save", handleMongooseError);
+
+exports.Water = model("water", waterSchema);
--- db/services/waterSerwices.js
+++ db/services/waterSerwices.js
@@ -1,98 +1,111 @@
-const { Types } = require('mongoose');
-const { HttpError } = require('../../helpers');
-const { Water } = require('../models/water');
+const { Types } = require("mongoose");
+const { HttpError } = require("../../helpers");
+const { Water } = require("../models/water");
 
 exports.add = async (data, owner) => {
-    const date = new Date();
-    const waterList = await Water.findOne({
-        date: { $gte: new Date(date.getFullYear(), date.getMonth(), date.getDate()) },
-        owner,
-    });
-    if (waterList) {
-        return Water.findByIdAndUpdate(
-            waterList._id,
-            {
-                $inc: { totalVolume: +data.waterVolume },
-                $push: { dailyEntries: data },
-            },
-            { new: true }
-        );
-    }
-    return Water.create({
-        date,
-        waterRate: owner.waterRate,
-        totalVolume: data.waterVolume,
-        dailyEntries: [data],
-        owner,
-    });
+  const date = new Date();
+  const waterList = await Water.findOne({
+    date: {
+      $gte: new Date(date.getFullYear(), date.getMonth(), date.getDate()),
+    },
+    owner,
+  });
+  if (waterList) {
+    return Water.findByIdAndUpdate(
+      waterList._id,
+      {
+        $inc: {
+          totalVolume: +data.waterVolume,
+        },
+        $push: { dailyEntries: data },
+        progress:
+          ((waterList.totalVolume + Number(data.waterVolume)) /
+            owner.waterRate) *
+          100,
+      },
+      { new: true }
+    );
+  }
+  return Water.create({
+    date,
+    waterRate: owner.waterRate,
+    totalVolume: data.waterVolume,
+    progress: (data.waterVolume / owner.waterRate) * 100,
+    dailyEntries: [data],
+    owner,
+  });
 };
 
-exports.getCurrentDay = async owner => {
-    const date = new Date();
-    const waterList = await Water.findOne({
-        date: { $gte: new Date(date.getFullYear(), date.getMonth(), date.getDate()) }, owner
-    });
-    if (!waterList) throw HttpError(404, 'No entries for the day')
-    return waterList;
+exports.getCurrentDay = async (owner) => {
+  const date = new Date();
+  const waterList = await Water.findOne({
+    date: {
+      $gte: new Date(date.getFullYear(), date.getMonth(), date.getDate()),
+    },
+    owner,
+  });
+  if (!waterList) throw HttpError(404, "No entries for the day");
+  return waterList;
 };
 
 exports.update = async (dayId, entryId, data) => {
-    const day = await Water.findById(dayId);
-    if (!day) throw HttpError(404, 'Day entry does not exist');
-    const entry = day.dailyEntries.find(i => i._id.toString() === entryId);
-    if (!entry) throw HttpError(404, 'Entry not found')
-    return Water.findOneAndUpdate(
-{ _id: dayId, 'dailyEntries._id': entryId },
-{
-    $inc: { totalVolume: +(data.waterVolume-entry.waterVolume) },
-            $set: {
-                'dailyEntries.$.waterVolume': data.waterVolume,
-                'dailyEntries.$.time': data.time,
-            },
-        }, {new: true}
-    );
-    // return Water.updateOne(
-        //     { _id: dayId, 'dailyEntries._id': entryId },
-        //     {
-    //         $inc: { totalVolume: +(data.waterVolume-entry.waterVolume) },
-    //         $set: {
-    //             'dailyEntries.$.waterVolume': data.waterVolume,
-    //             'dailyEntries.$.time': data.time,
-    //         },
-    //     }
-    // );
+  const day = await Water.findById(dayId);
+  if (!day) throw HttpError(404, "Day entry does not exist");
+  const entry = day.dailyEntries.find((i) => i._id.toString() === entryId);
+  if (!entry) throw HttpError(404, "Entry not found");
+  return Water.findOneAndUpdate(
+    { _id: dayId, "dailyEntries._id": entryId },
+    {
+      $inc: { totalVolume: +(data.waterVolume - entry.waterVolume) },
+      $set: {
+        "dailyEntries.$.waterVolume": data.waterVolume,
+        "dailyEntries.$.time": data.time,
+      },
+    },
+    { new: true }
+  );
+  // return Water.updateOne(
+  //     { _id: dayId, 'dailyEntries._id': entryId },
+  //     {
+  //         $inc: { totalVolume: +(data.waterVolume-entry.waterVolume) },
+  //         $set: {
+  //             'dailyEntries.$.waterVolume': data.waterVolume,
+  //             'dailyEntries.$.time': data.time,
+  //         },
+  //     }
+  // );
 };
 
 exports.remove = async (dayId, entryId) => {
-    const day = await Water.findById(dayId);
-    if (!day) throw HttpError(404, 'Day entry does not exist');
-    const entry = day.dailyEntries.find(i => i._id.toString() === entryId);
-    if (!entry) throw HttpError(404, 'Entry not found')
-    return Water.findByIdAndUpdate(
-        dayId,
-        {
-            $inc: { totalVolume: -entry.waterVolume },
-            $pull: { dailyEntries: { _id: entryId } },
-        },
-        { new: true }
-    );
+  const day = await Water.findById(dayId);
+  if (!day) throw HttpError(404, "Day entry does not exist");
+  const entry = day.dailyEntries.find((i) => i._id.toString() === entryId);
+  if (!entry) throw HttpError(404, "Entry not found");
+  return Water.findByIdAndUpdate(
+    dayId,
+    {
+      $inc: { totalVolume: -entry.waterVolume },
+      $pull: { dailyEntries: { _id: entryId } },
+    },
+    { new: true }
+  );
 };
 
 exports.getMonth = async (year, month, owner) => {
-    const selectedDates = await Water.find({
-        date: { $gte: new Date(year, month), $lt: new Date(year, month + 1) },
-        owner,
-    }).select(['-owner', '-updatedAt', '-createdAt']);
-    console.log("selectedDates:", selectedDates)
-    if (!selectedDates) throw HttpError(404, 'No entries for this month');
-    return selectedDates
-}
+  const selectedDates = await Water.find({
+    date: { $gte: new Date(year, month), $lt: new Date(year, month + 1) },
+    owner,
+  }).select(["-owner", "-updatedAt", "-createdAt"]);
+  console.log("selectedDates:", selectedDates);
+  if (!selectedDates) throw HttpError(404, "No entries for this month");
+  return selectedDates;
+};
 
-exports.checkById = id => {
-    const idIsValid = Types.ObjectId.isValid(id);
-    if (!idIsValid) throw HttpError(404, 'Not valid id');
+exports.checkById = (id) => {
+  const idIsValid = Types.ObjectId.isValid(id);
+  if (!idIsValid) throw HttpError(404, "Not valid id");
 };
 exports.checkIdByOwner = async (id, owner) => {
-    const isDayEntryExist = await Water.exists({ _id: id, owner });
-    if (!isDayEntryExist) throw HttpError(404, 'Day entry does not exist');
-}
+  const isDayEntryExist = await Water.exists({ _id: id, owner });
+  if (!isDayEntryExist) throw HttpError(404, "Day entry does not exist");
+};
