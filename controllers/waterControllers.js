d1598317c0021fb05ad32c6d3c29e29ac327b82e
Black Chaos <black.chaos.no1@gmail.com>
2024-02-06 11:28:48 +0200

    refactor: change structure controlls wrapper

--- controllers/waterControllers.js
+++ controllers/waterControllers.js
@@ -1,30 +1,30 @@
 const { waterServices } = require("../db/services");
 const { ctrlWrapper } = require("../helpers");
 
-exports.add = ctrlWrapper(async (req, res) => {
+const add = async (req, res) => {
   const dailyWater = await waterServices.add(req.body.date, req.body.water, req.user);
   dailyWater.owner = dailyWater.owner._id
   res.status(201).json(dailyWater)
-})
+}
 
-exports.getCurrentDay = ctrlWrapper(async (req, res) => {
+const getCurrentDay = async (req, res) => {
   const { date } = req.params;
   const dailyWater = await waterServices.getCurrentDay(date.replaceAll('-', ' '), req.user);
   if (!dailyWater) return res.json({dailyEntries: [], progress: 0})
   res.json(dailyWater)
-})
+}
 
-exports.update = ctrlWrapper(async (req, res) => {
+const update = async (req, res) => {
   const dailyWater = await waterServices.update(req.params.dayId, req.params.entryId, req.body);
   res.json(dailyWater)
-})
+}
 
-exports.remove = ctrlWrapper(async (req, res) => {
+const remove = async (req, res) => {
   const deleted = await waterServices.remove(req.params.dayId, req.params.entryId);
   res.json(deleted)
-})
+}
 
-exports.getMonth = ctrlWrapper(async (req, res) => {
+const getMonth = async (req, res) => {
   const date = new Date();
   const { user, query: { year = date.getFullYear(), month = date.toLocaleString('en', {month: 'long'})} } = req;
   let selectedDates = await waterServices.getMonth(year, month, user);
@@ -36,4 +36,12 @@ exports.getMonth = ctrlWrapper(async (req, res) => {
       dailyEntries: i.dailyEntries.length,}
   });
   res.json(selectedDates)
-})
+}
+
+module.exports = {
+  add: ctrlWrapper(add),
+  getCurrentDay: ctrlWrapper(getCurrentDay),
+  update: ctrlWrapper(update),
+  remove: ctrlWrapper(remove),
+  getMonth: ctrlWrapper(getMonth),
+}
